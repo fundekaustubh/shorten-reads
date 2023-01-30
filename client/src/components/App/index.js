89652fecef3e96ee97ffc4754919b5da0b3b8b61
Kaustubh <fundekaustubh@gmail.com>
2023-01-30 19:27:19 +0530

    Completed fetching of appropriate articles from the backend. Installed axios in frontend and backend for HTTP requests, handled async fetching of articles from backend by displaying loading, success and error statuses, styled frontend article display section by adding a bit of margin and removing centre alignment (have to style more later), whitelisted frontend URL inside CORS for the backend server, designed an API endpoint for querying articles from the frontend, installed news API client on the backend.

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,30 +1,56 @@
 import './styles.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const searchText = useRef(null);
   const searchStartingDate = useRef(null);
   const searchEndingDate = useRef(null);
   const searchType = useRef(null);
+  const [searchBody, setSearchBody] = useState({});
+  const [articles, setArticles] = useState(undefined);
+  const [fetchArticlesIsPending, setFetchArticlesIsPending] = useState(true);
+  const [fetchArticlesError, setFetchArticlesError] = useState(undefined);
+
+  /**
+   * @function
+   * @async
+   * @description Asynchronously fetch articles from the backend server. 
+   * The function sets the fetch status and fetched articles data on success, and sets an error on failure. 
+   */
+  const fetchArticles = async () => {
+    try {
+      setFetchArticlesIsPending(true);
+      const _articles = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/articles`, searchBody);
+      setArticles(_articles.data.articles);
+      setFetchArticlesIsPending(false);
+      setArticles(_articles);
+    }
+    catch (err) {
+      setFetchArticlesError(err);
+      setFetchArticlesIsPending(false);
+    }
+  }
+
+  useEffect(() => {
+    setArticles([]);
+    fetchArticles();
+  }, [searchBody])
 
   /**
  * Handles the search functionality and returns a list of articles.
  * 
  * @param {Object} evt - The event object passed in by the form submission.
- * @returns {Array} articles - The list of articles fetched from the news API.
  */
   const handleSearch = async (evt) => {
     evt.preventDefault();
     // include sources as well
-    const searchBody = {
+    setSearchBody({
       q: searchText.current.value,
       from: searchStartingDate.current.value,
       to: searchEndingDate.current.value,
       sortBy: searchType.current.value
-    };
-    // these articles are fetched after an HTTP call to news API
-    const articles = [];
-    return articles;
+    });
   }
 
   return (
@@ -34,12 +60,35 @@ function App() {
         <input placeholder="From" type="date" ref={searchStartingDate} />
         <input placeholder="To" type="date" ref={searchEndingDate} />
         <select ref={searchType}>
-          <option value="popularity" selected>Popularity</option>
+          <option value="popularity" defaultValue>Popularity</option>
           <option value="relevancy">Relevancy</option>
           <option value="publishedAt">Published At</option>
         </select>
         <button>Submit</button>
       </form>
+      {
+        fetchArticlesIsPending &&
+        <div>
+          Fetching articles...
+        </div>
+      }
+      {
+        articles && articles.data && articles.data.articles.length > 0 &&
+        <p>{articles.data.articles.length} articles found...</p> &&
+        <ul>
+          {articles.data.articles.map((article, idx) => {
+            return articles.author ?
+              <li key={idx}>
+                {article.description},
+                {article.author}
+              </li> :
+              <li key={idx}>
+                {article.description}
+              </li>
+          }
+          )}
+        </ul>
+      }
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -3,11 +3,36 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 const app = express();
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWS_API_API_KEY);
+const axios = require('axios');
+const cors = require('cors');
+const CORSWhitelist = [process.env.FRONTEND_URL];
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: function (origin, callback) {
+        const originIsWhitelisted = CORSWhitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    }
+}))
 
+app.post('/articles', async (req, res) => {
+    try {
+        // Request body contains the query in the form of a JSON object, which will be sent to the news API.
+        const newsAPIResponse = await newsapi.v2.everything({
+            ...req.body,
+            language: 'en'
+        });
+        return res.json(newsAPIResponse);
+    }
+    catch (err) {
+        console.log("Error: ", err);
+        return res.status(500).send(err);
+    }
+})
 
 app.listen(process.env.BACKEND_PORT,
     /**
